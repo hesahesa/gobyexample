2293948c87f70c58834de9121aed1b037946b374
Prahesa K Setia <prahesa@yahoo.com>
2020-05-16 07:34:39 +0700

    update hello-world

--- examples/hello-world/hello-world.sh
+++ examples/hello-world/hello-world.sh
@@ -1,5 +1,5 @@
-# Untuk menjalankan program, letakkan kode di file `hello-world.go` dan
-# gunakan `go run`.
+# Untuk menjalankan program, letakkan kode di file 
+# `hello-world.go` dan gunakan `go run`.
 $ go run hello-world.go
 hello world
 
@@ -9,9 +9,11 @@ $ go build hello-world.go
 $ ls
 hello-world	hello-world.go
 
-# Kita lalu bisa menjalankan binary yang telah di-build secara langsung.
+# Kita lalu bisa menjalankan binary yang telah di-build 
+# secara langsung.
 $ ./hello-world
 hello world
 
-# Sekarang kita telah bisa menjalankan dan mem-build program Go sederhana, mari
-# belajar lebih lanjut tentang bahasa ini.
+# Sekarang kita telah bisa menjalankan dan mem-build 
+# program Go sederhana, mari belajar lebih lanjut 
+# tentang bahasa ini.
